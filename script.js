c83b28e0409690b8f6c0d1aaf1d66c7d2bad2107
Mizzet18 <architectofthought18@gmail.com>
2020-09-05 21:11:56 -0500

    Updated Script.js logic

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+    let container = document.getElementById('container');
+    let astronauts = '';
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
+        response.json().then(function(teamlist){
+            for(let i = 0; i < teamlist.length; i++){
+                astronauts += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${teamlist[i].firstName} ${teamlist[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${teamlist[i].hoursInSpace}</li>
+                            <li>Active: false</li>
+                            <li>Skills: Physician, Chemical Engineer</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="images/mae-jemison.jpg">
+                </div>
+                `
+            }
+            container.innerHTML = astronauts;
+        })
+    })
+})
